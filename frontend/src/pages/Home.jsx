8e7d8dfad5b49022ba2ffeda0b8913f8b7f467a3
sreeja126 <nainaboinasreeja74@gmail.com>
2026-07-29 15:12:47 +0530

    Initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        ShopStack
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
